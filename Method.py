d06eb535ab9d4564d9ec0f3b21876d3922fd633d
Ying Cai <cai7@g.clemson.edu>
2018-12-04 23:14:39 -0500

    Update Method.py

--- Method.py
+++ Method.py
@@ -3,6 +3,7 @@ Created on Dec 4, 2018
 
 @author: Ying
 '''
+from Cosine import similarity_matrix
 
 
 
@@ -20,8 +21,10 @@ def recall_precision (pred_list, real_list, recall, precision):
     precision = count / pred_count
     
     
-def order_list (similarity_list, movieIndex):
-    indices = []
+def select_movie_basedon_similarity (similarity_matrix, movieId):
+    movieIndex = #translate here
+    IDs = []
+    similarity_list = similarity_matrix[movieIndex,:] ##??correct
     size = similarity_list.shape[0]
     limit = 1.09
     
@@ -29,9 +32,10 @@ def order_list (similarity_list, movieIndex):
         limit = limit - 0.1
         for i in range (0, size):
             if similarity_list[i] >= limit and i != movieIndex:
-                indices.append(i)
+                m_ID = #translate back here
+                IDs.append(m_ID)
     
-    return indices
+    return IDs
 
 def pred_list (movieId_list, movie_user_dict): ##Here must user movieId not index
     user_list = []
@@ -44,10 +48,20 @@ def pred_list (movieId_list, movie_user_dict): ##Here must user movieId not inde
     user_list = list(set(user_list))
     return user_list
 
-def selected_watched_list(userId, user_movie_dict):
+def selected_watched_list(userId, user_movie_dict, movieId_index_trans, similarity_matrix):
+    rec_list=[]
+    select_movie = []
     watched_list = user_movie_dict.get(str(userId))
     mean = sum(watched_list.values()) / float(len(watched_list))
-
+    for key in watched_list:
+        if watched_list.get(key) >= mean:            
+            select_movie.extend(select_movie_basedon_similarity(similarity_matrix, key))
+    select_movie = list(set(select_movie))
+    
+    if len(select_movie) > 50: #recommend 50 movies or 100
+        
+        
+    
 
     
     
